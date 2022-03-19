14937478dcc35d8b19fbe5c21c4686eab96e3d57
Lokinder Singh <72655298+lokiinderr@users.noreply.github.com>
2022-03-19 18:38:29 +0530

    Update valuekeys.js

--- config/valuekeys.js
+++ config/valuekeys.js
@@ -1,8 +1,3 @@
-// module.exports={
-//     MONGOURI:"mongodb+srv://lokiinderr:0056bdd12312@cluster0.iemuv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-//     //JWT SECRET is just random string of number and alphabet
-//     JWT_SECRET:"q1w2e3r4t5efhgjiaunva35drfge4gsdddh32"
-// }
 
 if(process.env.NODE_ENV=="production"){
     module.exports=require('./prod')
