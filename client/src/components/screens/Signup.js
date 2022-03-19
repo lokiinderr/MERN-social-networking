aa275a5711b48a2b6c33550959aa9cacf3ec1536
Lokinder Singh <72655298+lokiinderr@users.noreply.github.com>
2022-03-19 23:50:45 +0530

    Update Signup.js

--- client/src/components/screens/Signup.js
+++ client/src/components/screens/Signup.js
@@ -23,7 +23,7 @@ export const Signup = () => {
     data.append("file",image);
     data.append("upload_preset","socialnetworking");
     data.append("cloud_name","lokindercloud")
-    fetch('https://api.cloudinary.com/v1_1/lokindercloud/image/upload',{
+    fetch('https://api.cloudinary.com/<YOUR CLOUDINARY API>',{
         method:"post",
         body:data
     }).then(res=>res.json())
